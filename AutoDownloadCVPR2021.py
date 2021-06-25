469871c7715e30a64a28c7feb8bf86358cbe98f0
adakoda <adakoda.jamjampan@gmail.com>
2021-06-25 15:18:19 +0900

    apply code formatter

--- AutoDownloadCVPR2021.py
+++ AutoDownloadCVPR2021.py
@@ -4,18 +4,20 @@
 """
 
 import os
-import requests
 import time
 import urllib.robotparser
+
+import requests
 
 conf = 'CVPR2021'
 header = 'http://openaccess.thecvf.com/'
 
+
 def name_check(name):
-    name = name.replace('?','')
-    name = name.replace(':','')
-    name = name.replace('*','')
-    name = name.replace('/',' or ')
+    name = name.replace('?', '')
+    name = name.replace(':', '')
+    name = name.replace('*', '')
+    name = name.replace('/', ' or ')
     return name
 
 
@@ -41,26 +43,27 @@ def main():
     crawl_delay_sec = get_crawl_delay_sec(header, header + conf)
     if crawl_delay_sec == -1:
         exit()
-    r = requests.get(header+conf,
+    r = requests.get(header + conf,
                      headers={'User-Agent': 'AutoDownloadCVPR2021'},
                      params={'day': 'all'})
     txt = r.text
     lines = txt.split('\n')
     cnt = 0
     for line in lines:
-        if line.find('<dt class="ptitle">')>-1:
-            pdfname = conf+'/'+name_check(line.split('>')[3].split('<')[0])+'.pdf'
-            cnt+=1
-        if len(line)>0:
-            if line[0]=='[':
-                print(str(cnt)+':'+pdfname)
+        if line.find('<dt class="ptitle">') > -1:
+            pdfname = conf + '/' + name_check(line.split('>')[3].split('<')[0]) + '.pdf'
+            cnt += 1
+        if len(line) > 0:
+            if line[0] == '[':
+                print(str(cnt) + ':' + pdfname)
                 if not os.path.exists(pdfname):
-                    url = header+line.split('"')[1]
+                    url = header + line.split('"')[1]
                     r = requests.get(url)
                     f = open(pdfname, 'wb')
                     f.write(r.content)
                     f.close()
                     time.sleep(crawl_delay_sec)
 
+
 if __name__ == '__main__':
-    main()        
+    main()
